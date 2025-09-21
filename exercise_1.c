b96eaa8a20f9165106d6dcc313164db182641d5f
scaredofthesix <bodulevmax@gmail.com>
2025-09-21 17:10:24 +0300

    Update exercise_1.c

--- exercise_1.c
+++ exercise_1.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
 
+int main(){
+    int n,k = 1,y = 0;
+    scanf("%d",&n);
+    for (int i = n; i >= 0 ;i--){     // made upside-down triangle of " "
+        for (int j = 0; j < i ;j++){
+            printf(" ");
+        }
+        for (int g = 0;g < y;g++){ // called for(...) with g < y where y = number of string
+            printf("%d ",k);
+            k = k + 1;
+        }
+        y = y + 1;
+        printf("\n");
+    } 
+}
